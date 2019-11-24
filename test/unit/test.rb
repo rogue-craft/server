bb2a5f00d1a8f953d080af092447ad1dc1dfbdce
Farkasvölgyi <isty001@gmail.com>
2019-11-24 20:36:25 +0100

    codecov

--- test/unit/test.rb
+++ test/unit/test.rb
@@ -10,7 +10,7 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require_relative "../../lib/rogue-craft-server/container_loader"
 
-require "minitest/autorun"
+require 'minitest/autorun'
 require 'mocha/minitest'
 
 Dependency.container.config.resolver = -> (_, _) { nil }
