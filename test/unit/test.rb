25479e4ade97d50b0582a6d6dd84b4933a839dcc
Isty001 <isty001@gmail.com>
2020-01-05 20:42:43 +0100

    travis

--- test/unit/test.rb
+++ test/unit/test.rb
@@ -1,9 +1,6 @@
 if ENV['GENERATE_COVERAGE']
   require 'simplecov'
   SimpleCov.start
-
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
