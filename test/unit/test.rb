798af2b633cbf6f9fb59eedcae73fc46c63779d8
Isty001 <isty001@gmail.com>
2020-01-05 21:37:57 +0100

    travis

--- test/unit/test.rb
+++ test/unit/test.rb
@@ -1,16 +1,13 @@
 if ENV['GENERATE_COVERAGE']
   require 'simplecov'
   SimpleCov.start
-
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require_relative "../../lib/rogue-craft-server/container_loader"
 
-require "minitest/autorun"
+require 'minitest/autorun'
 require 'mocha/minitest'
 
 Dependency.container.config.resolver = -> (_, _) { nil }
